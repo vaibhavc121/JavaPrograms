aaeba0db31690092bc0df9c3109e0951674072de
vaibhav <vaibhavc121@gmail.com>
2025-10-17 23:38:07 +0530

    17-10

--- src/test/java/practice/RemoveDuplicateFromArray.java
+++ src/test/java/practice/RemoveDuplicateFromArray.java
@@ -1,5 +1,7 @@
 package practice;
 
+import java.util.Arrays;
+
 public class RemoveDuplicateFromArray
 {
     public static void main(String[] args)
@@ -7,14 +9,16 @@ public class RemoveDuplicateFromArray
         int[] no={1,2,3,1,4,5,5};
         for(int i=0;i<no.length-1;i++)
         {
-            for(int j=i+1;j<no.length;j++)
+            for(int j=i+1;j<no.length-2;j++)
             {
                 if(no[i]==no[j])
                 {
-                    
+                    no[j]=no[j+1];
                 }
             }
         }
+
+        System.out.println(Arrays.toString(no));
     }
 
 }
