dc04f199169995a6ce3c3ced4d7070d43f3f3e6e
vaibhav <vaibhavc121@gmail.com>
2025-12-10 23:28:48 +0530

    10-12

---
+++ src/test/java/practice/SecondLargestNum.java
@@ -0,0 +1,27 @@
+package practice;
+
+public class SecondLargestNum
+{
+    public static void main(String[] args)
+    {
+        int[] arr={1,2,3,4,5};
+        int largest=Integer.MIN_VALUE;
+        int secondLargest=Integer.MIN_VALUE;
+
+        for(int num:arr)
+        {
+            if(num>largest)
+            {
+                secondLargest=largest;
+                largest=num;
+            }
+            else if(num>secondLargest && num !=largest)
+            {
+                secondLargest=num;
+            }
+        }
+        System.out.print(secondLargest);
+
+        //test
+    }
+}
